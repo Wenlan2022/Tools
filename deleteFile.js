912bb138109e041d8f16555a15ec6d659277c6d8
wenlan <wenlan.tan1@gmail.com>
2023-09-10 19:30:06 -0500

    allow user to enter the path and the file type

--- deleteFile.js
+++ deleteFile.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
+const prompt = require("prompt-sync")({sigint:true});
 
-const folderPath = 'path'  // replace "path" with the path of the folder
+// const folderPath = 'path'  // replace "path" with 
+
+const folderPath = prompt("Please enter the path of your folder: ") // enter the path of the folder
+const type = prompt("Please enter the type of files you want to delete: ") // enter the file type you want to delete
 
 
 try {
@@ -11,7 +15,7 @@ try {
     console.log(file)
     console.log('*****************************************')
 
-    let fileType = file.filter((fileName) => fileName.includes('type')) // replce 'type' with the file type you want to delete
+    let fileType = file.filter((fileName) => fileName.includes(type)) 
     console.log('-----------------------------------------')
     console.log('Here is the file is going to delete: ', fileType)
     console.log('-----------------------------------------')
@@ -43,3 +47,20 @@ try {
 }
 
 
+// function logUserName() {
+//     // Use the prompt function to get user input
+//     const userName = prompt("Please enter your name:");
+  
+//     // Check if the user entered a name
+//     if (userName !== null && userName !== "") {
+//       // Log the entered name to the console
+//       console.log("Hello, " + userName + "!");
+//     } else {
+//       // Handle the case where the user didn't enter a name
+//       console.log("You did not enter a name.");
+//     }
+//   }
+  
+//   // Call the function to start the process
+//   logUserName();
+  
